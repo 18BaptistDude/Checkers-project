1bdec9a95eeb603ef1d8157fd742a5d39363e91c
Luke Smith <lasmithart18@gmail.com>
2019-02-06 14:29:20 -0500

    made a javascript template.

--- template.js
+++ template.js
@@ -1,1 +1,26 @@
-templat
+var yummyFood= `bacon`
+console.log(`some stuff in: ${yummyFood}
+more stuff in: ${yummyFood}
+<div id="row-1" class="row">
+    <div id="cell-1-1" class="cell crimson"></div>
+    <div id="cell-1-2" class="cell black" onclick="toggle(this)">
+      <div class="checker red-checker"></div>
+      <div class="checker grey-checker" hidden></div>
+    </div>
+    <div id="cell-1-3" class="cell crimson"></div>
+    <div id="cell-1-4" class="cell black" onclick="toggle(this)">
+      <div class="checker red-checker"></div>
+      <div class="checker grey-checker" hidden></div>
+    </div>
+    <div id="cell-1-5" class="cell crimson"></div>
+    <div id="cell-1-6" class="cell black" onclick="toggle(this)">
+      <div class="checker red-checker"></div>
+      <div class="checker grey-checker" hidden></div>
+    </div>
+    <div id="cell-1-7" class="cell crimson"></div>
+    <div id="cell-1-8" class="cell black" onclick="toggle(this)">
+      <div class="checker red-checker"></div>
+      <div class="checker grey-checker" hidden></div>
+    </div>
+  </div>`)
+
